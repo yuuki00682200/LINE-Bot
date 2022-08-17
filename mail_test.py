7afe1798708f7762983fe6e99fbb2c42b10127df
yuuki00682200 <yukitakatsu6468@gmail.com>
2022-08-17 23:48:43 +0900

    first commit

---
+++ mail_test.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+def create_message(from_addr, to_addr, subject, body):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    return msg
+
+
+def send_mail(body_msg):
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.ehlo()
+    smtpobj.starttls()
+    smtpobj.ehlo()
+    smtpobj.login("kotabbcc@gmail.com", "Kota0116")
+    a = smtpobj.sendmail(body_msg["From"], body_msg["To"], body_msg.as_string())
+    smtpobj.close()
+
+body = create_message("kotabbcc@gmail", "kouta_19n1101637@nnn.ed.jp", "Test", "test")
+send_mail(body)
